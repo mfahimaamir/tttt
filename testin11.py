a0368f5c5d3f441be30207e6ff48bbed4dfc6079
mfahimaamir <mfahim204s8@gmail.com>
2025-01-08 20:11:20 +0500

    Add files via upload

---
+++ testin11.py
@@ -0,0 +1,20 @@
+import streamlit as st
+st.set_page_config("Youtube Data Harvesting", layout = "wide", page_icon=r'Related Images and Videos/youtube.png')
+
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+
+
+st.header("Muhammad is the Best in all over the universes")
